bcae8628cdc89e57335887a91771a43c4b594dad
Florian Poppinger <florianpoppinger@gmail.com>
2024-02-23 09:55:14 +0100

    ka

--- MTCG/MTCG/obj/Debug/net7.0/MTCG.AssemblyInfo.cs
+++ MTCG/MTCG/obj/Debug/net7.0/MTCG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e33ca2ee57f76a0f14e2b794da6534f689aa27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef9c2f8aaf7f8ab0b20760b02a0e42620c3c6fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTCG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
